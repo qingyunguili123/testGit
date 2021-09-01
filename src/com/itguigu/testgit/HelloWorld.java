654c44e801fc4ac6fa46befbc164eb91b63d1ce4
WY_LXL <wy_love_lxl@2018.com>
2021-09-01 14:58:58 +0800

    协同开发第二次提交

--- src/com/itguigu/testgit/HelloWorld.java
+++ src/com/itguigu/testgit/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("测试协同开发第1次提交");
+        System.out.println("测试协同开发第2次提交");
     }
 }
