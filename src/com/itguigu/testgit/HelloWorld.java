a6df9d42187c374c6b0d8742b2a68ea5bbbff459
WY_LXL <wy_love_lxl@2018.com>
2021-09-01 15:03:13 +0800

    协同开发第1次冲突测试

--- src/com/itguigu/testgit/HelloWorld.java
+++ src/com/itguigu/testgit/HelloWorld.java
@@ -8,5 +8,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("测试协同开发第1次提交");
         System.out.println("测试协同开发第2次提交");
+        System.out.println("测试协同开发冲突1");
     }
 }
