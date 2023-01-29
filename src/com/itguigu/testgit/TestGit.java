f72769eadd657033723ec9117fa95ce1a14162b6
WY_LXL <wy_love_lxl@2018.com>
2023-01-29 21:31:38 +0800

    测试合并分支develop

--- src/com/itguigu/testgit/TestGit.java
+++ src/com/itguigu/testgit/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("测试Git版本控制");
         System.out.println("测试第一次修改");
+        System.out.println("测试第二次修改");
     }
 }
